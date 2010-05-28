ad16cfc07fa1feffae9c05d4285f5aba1a8d688b
Kevin Gustavson <info@fvts.net>
2010-05-28 16:26:49 -0500

    Content for 'Who are we?' and 'I'd like to talk to someone' pages.

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="google-site-verification" content="1hkL-yUG1b7r6JKQU-kMsvVItqt6s8r4MQjGKW8PyoA" />
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 <title>Fox Valley Theological Society</title>
 </head>
