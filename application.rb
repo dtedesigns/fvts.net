4e2bbeac0fcec4b6c483d86fed5e54529a9e14fb
Kevin Gustavson <kgust@pobox.com>
2011-04-24 19:27:28 -0500

    Add Sinatra files: 1. Main config (application.rb) 2. Views 3. Restart file (tmp/restart.txt)

---
+++ application.rb
@@ -0,0 +1,71 @@
+# routes.rb
+
+require 'sinatra'
+require 'less'      # LESS CSS templates
+require 'haml'
+require 'erb'
+require 'rdiscount' # for markdown
+require 'redcloth'  # for textile
+
+set :layout_engine => :erb
+set :textile, :layout_engine => :erb
+
+# Examples
+post '/' do
+    # create something
+end
+
+
+put '/' do
+    # replace something
+end
+
+#patch '/' do
+    # modify something
+#end
+
+delete '/' do
+    # annihilate something
+end
+
+options '/' do
+    # appease something
+end
+
+
+get '/hello/:name' do |n|
+    "Hello #{n}!"
+end
+
+get '/haml' do
+    haml :example
+end
+
+get '/erb' do
+    erb :example
+end
+
+get '/markdown' do
+    markdown :example
+end
+
+get '/textile' do
+    textile :example
+end
+
+# Fox Valley Theological Society
+get '/' do
+    erb :home
+end
+
+get '/who_are_we' do
+    textile :who_are_we
+end
+
+get '/events' do
+	erb :events
+end
+
+get '/talk_to_me' do
+    textile :talk_to_me
+end
