85194efc75fadd8d804cd89eb12227df8f2a7a02
Kevin Gustavson <kgustavson@celltrak.com>
2011-08-27 21:22:03 -0500

    Updates to website (mostly non-visible).
    1. Converted events for a separate bio/description format to yaml/template driven.
    2. Removed some of the cruft in application.rb and the example files.
    3. Using liquid format files where possible. They are more compact and readable.

--- application.rb
+++ application.rb
@@ -2,13 +2,31 @@
 
 require 'sinatra'
 require 'less'      # LESS CSS templates
-require 'haml'
 require 'erb'
-require 'rdiscount' # for markdown
+require 'liquid'
 require 'redcloth'  # for textile
+require 'partial_helper'
 
 set :layout_engine => :erb
+#set :erb, :layout_engine => :erb
 set :textile, :layout_engine => :erb
+set :liquid, :layout_engine => false
+
+# use Rack::Auth::Basic "Restricted Area" do |username, password|
+#     [username, password] == ['admin','admin']
+# end
+
+def authorized?
+    @auth ||= Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? && @auth.basic? && @auth.credentials == ['admin','admin']
+end
+
+def protected!
+    unless authorized?
+        response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+        throw(:halt, [401, "Not authorized\n"])
+    end
+end
 
 # Examples
 post '/' do
@@ -33,29 +51,21 @@ options '/' do
 end
 
 
-get '/hello/:name' do |n|
-    "Hello #{n}!"
-end
-
-get '/haml' do
-    haml :example
-end
-
-get '/erb' do
-    erb :example
-end
-
-get '/markdown' do
-    markdown :example
-end
-
-get '/textile' do
-    textile :example
-end
-
 # Fox Valley Theological Society
 get '/' do
-    erb :home
+    erb :home, :locals => {
+        :data => YAML.load_file('views/event/2011-05-21.yml'),
+        :sidebar => erb(
+            :sidebar,
+            :layout => false,
+            :locals => {
+                :data => [
+                     YAML.load_file('views/event/2011-05-21.yml'),
+                     YAML.load_file('views/event/2011-04-09.yml')
+                ]
+            }
+        )
+    }
 end
 
 get '/who_are_we' do
@@ -63,7 +73,17 @@ get '/who_are_we' do
 end
 
 get '/events' do
-	erb :events
+    upcoming = [ liquid :event_item, :layout => false, :locals => { :data => YAML.load_file('views/event/2011-05-21.yml') } ]
+    previous = [
+        liquid(:event_item, :layout => false, :locals => { :data => YAML.load_file('views/event/2011-04-09.yml') } ),
+        liquid(:event_item, :layout => false, :locals => { :data => YAML.load_file('views/event/2011-02-26.yml') } ),
+        liquid(:event_item, :layout => false, :locals => { :data => YAML.load_file('views/event/2010-12-04.yml') } ),
+    ]
+
+    liquid :events, :layout_engine => :erb, :locals => {
+        :upcoming => upcoming,
+        :previous => previous,
+    }
 end
 
 get '/talk_to_me' do
@@ -72,10 +92,20 @@ end
 
 # Possible stylesheet route
 get '/style/:name' do |n|
-	less :"style/#{n}"
+    less :"style/#{n}"
 end
 
 get '/default.css' do
-	#"put template here"
-	less :"style/default"
+    #"put template here"
+    less :"style/default"
+end
+
+get '/test' do
+    protected!
+    str = ''
+    Dir.foreach('views/event') { |f|
+        next if f.index('.') == 0
+        str << "#{f}<br>"
+    }
+    str
 end
